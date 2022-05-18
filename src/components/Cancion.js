631813b1ddff6dc5bbe5c928180be1161dcb88e1
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2022-05-18 15:38:03 -0500

    lyrics of fire

--- src/components/Cancion.js
+++ src/components/Cancion.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const Cancion = ({ letra }) => (
   <Fragment>
     <h2>Letra Canción</h2>
-    <p className="letra">{ketra}</p>
+    <p className="letra">{letra}</p>
   </Fragment>
 )
 
