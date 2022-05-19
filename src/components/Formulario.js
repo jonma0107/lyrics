2369f653096a39f6c645a84d3dc8d8186ed72a5b
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2022-05-18 23:46:44 -0500

    lyrics of fire

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -40,7 +40,7 @@ const Formulario = ({ guardarBusquedaLetra }) => {
             className="col card text-white bg-transparent mb-5 pt-5 pb-2">
 
             <fieldset>
-              <legend className="text-center">Buscador Letras Canciones</legend>
+              <legend className="text-center">Buscador Letras Canciones & Artistas</legend>
 
               <div className="row">
                 <div className="col-md-6">
